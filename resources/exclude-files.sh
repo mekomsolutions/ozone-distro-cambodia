153c06f8c10bd35df5176328526bcf639b69914b
Dimitri R <dimitri@mekomsolutions.com>
2022-12-19 21:14:03 +0100

    Improved comments in exclude-files.sh.

--- resources/exclude-files.sh
+++ resources/exclude-files.sh
@@ -5,7 +5,7 @@ targetDir=$1
 cat dependency-excludes.txt | while read line || [ -n "$line" ]
 do
   if [ "${line:0:1}" != "#" ]; then
-    echo "Evaluting regex \"$line\" for possible matching exclusions..."
+    echo "Evaluating RegEx \"$line\" for possible matching exclusions..."
     count=$(find ${targetDir} -regex ${line} | wc -l)
     if [ "$count" -ne "0" ]; then 
       echo "Found the following file(s): "
@@ -13,7 +13,7 @@ do
       find ${targetDir} -regex ${line} | xargs rm -r
       echo "$count file(s) excluded."
     else
-      echo "No file to exclude."
+      echo "No files were matched to be excluded."
     fi
   fi
 done
