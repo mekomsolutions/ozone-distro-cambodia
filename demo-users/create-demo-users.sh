a6882851f94d3f97649cc67aec4369b758736416
rbuisson <romain@mekomsolutions.com>
2023-03-28 19:10:16 +0200

    KH-118: Some adjustements to the script

--- demo-users/create-demo-users.sh
+++ demo-users/create-demo-users.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
+set -e
 
 # Install nvm
 VERSION=v0.37.2
 curl -o- "https://raw.githubusercontent.com/nvm-sh/nvm/${VERSION}/install.sh" | bash
+source ~/.nvm/nvm.sh
 nvm install --lts
-npm install
 
-# Run app to create users
+# Run app to create the demo users
+npm install
 node app.js
-exit 0
